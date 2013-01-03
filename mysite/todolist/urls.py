e04589c51d92e3f3269d48711f62ed8eee3a874d
Shishupal Kumar <shishupal.kumar@hashedin.com>
2013-01-03 12:00:29 +0530

    modified front end design

--- mysite/todolist/urls.py
+++ mysite/todolist/urls.py
@@ -3,17 +3,22 @@ from todolist import views
 from django.conf import settings
 
 urlpatterns = patterns('',
+    
+    url(r'^anurag/$', views.anurag, name='anurag'),
+    
+    url(r'^results/(\d+)/$', views.results, name='results'),
+    url(r'^results/(\d+)$', views.results, name='results'),
+    
+    url(r'^(?P<todolist_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<todolist_id>\d+)$', views.detail, name='detail'),
+    
+    #********************Project Start******************************************
+    
     url(r'^$', views.index, name='index'),
     
     url(r'^collaborator$', views.collaborator, name='collaborator'), 
     
     url(r'^task$', views.task, name='task'),
-    
-    #url(r'^$', views.mystyle, name='bootstrap'),
-    
-    url(r'^anurag/$', views.anurag, name='anurag'),
-    
-    url(r'^results/(\d+)/$', views.results, name='results'),
    
 )
 
